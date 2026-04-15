fe0b7ddb4934bf90f1c9ede931e876a1c9633a55
SamipyaPhuyal <samipyaphuyal012@gmail.com>
2026-04-15 20:41:02 +0545

    serializers

--- Jobs/api/serializers.py
+++ Jobs/api/serializers.py
@@ -7,7 +7,7 @@ class JobSerializer(serializers.ModelSerializer):
     posted_by = serializers.SerializerMethodField()
     class Meta:
         model = Job
-        fields ="__all__"
+        fields =["position","company","location","description","requirements","type","posted_by","total_applications"]
         read_only_fields = ["posted_by","total_applications"]
     def get_posted_by(self, obj):
         return obj.posted_by.username
